11b6d4061408a8fd04e30f140d6b8c398bb21de8
he <he529564582@163.com>
2025-03-19 18:08:57 +0800

    add all

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, between
+
+
+class StressTestUser(HttpUser):
+    wait_time = between(0.5, 1)  # 每个请求后等待时间：0.5到1秒之间随机
+
+    @task
+    def catlist_page(self):
+        self.client.get("/playlist/catlist")  # 请求接口1
+
+    @task
+    def hot_page(self):
+        self.client.get("/playlist/hot")  # 请求接口2
